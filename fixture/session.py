f45ec3483a28a5cf2bbe4623291c9c93a8d6bd43
Badin1 <66058511+Badin1@users.noreply.github.com>
2021-02-10 18:42:13 +0300

    ошибка в задании 25: Процесс завершен с кодом выхода 0

---
+++ fixture/session.py
@@ -0,0 +1,54 @@
+class SessionHelper:
+
+# PYTHONUNBUFFERED=1 в Edit Conf
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_xpath("//input[@value='Войти']").click()
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Войти']").click()
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("administrator").click()
+        wd.find_element_by_link_text(" Выход").click()
+        wd.find_element_by_xpath("//input[@value='Войти']")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_link_text("administrator").text
+
+
+
+
